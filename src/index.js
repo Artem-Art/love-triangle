622df38f0b07d01d88476f75748d4eeb2e1f235d
artfront <krivonosart19@gmail.com>
2019-03-19 13:15:20 +0300

    solve task in index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let count = 0;
+    const arr = [];
+    arr[0] = 0;
+    for (let i = 0; i <= preferences.length; i++) {
+        arr[i + 1] = preferences[i];
+    }
+    for (let i = 1; i <= (arr.length - 2); i++) {
+        if (arr[arr[arr[i]]] == i && arr[i] != i) { count++; }
+    }
+    return count / 3;
+
 };
